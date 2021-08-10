25e159079b84d6b7ed88804629478d1e2029c109
IhorHrytsenko <gisyshka@gmail.com>
2021-08-10 10:45:29 +0300

    hw-11

---
+++ src/js/color-switch.js
@@ -0,0 +1,35 @@
+const startB = document.querySelector('button[data-action=start]');
+const stopB = document.querySelector('button[data-action=stop]');
+const bodyTag = document.querySelector('body');
+let timerColorId;
+isRun = false;
+
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function changeColor(colors = [], random){
+    bodyTag.style.backgroundColor = colors[random(0, colors.length - 1)];
+    console.log(bodyTag.style.backgroundColor);
+}  
+
+startB.addEventListener('click', 
+() => {
+    if (isRun) {return}
+    timerColorId = setInterval(changeColor, 1000, colors, randomIntegerFromInterval);
+    isRun = true;
+});
+
+stopB.addEventListener('click', () => {
+    clearInterval(timerColorId);
+    isRun = false;
+})
