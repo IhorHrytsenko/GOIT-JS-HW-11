c505d8f384067bee939326593859aa0b26ec875e
IhorHrytsenko <gisyshka@gmail.com>
2021-08-12 16:41:10 +0300

    timer-class-done

--- src/js/timer.js
+++ src/js/timer.js
@@ -5,55 +5,65 @@ const refs = {
     secs: document.querySelector("span[data-value=secs]"),
 }
 
-const timer = {
+class Timer {
+    constructor(day = 1, hour = 0, min = 0, sec = 0, MINUS = 1000){
 
-    startDate : new Date(),
-    endDate : new Date(),
-    deltaTime: this.endDate - this.startDate,
-    MINUS: 1000,
+    this.day = day;
+    this.hour = hour;    
+    this.minutes = min;
+    this.seconds = sec;
 
-    timeComponent(deltaTime) {
+    this.startDate = new Date(),
+    this.endDate = new Date(),
+    this.deltaTime = this.endDate - this.startDate,
+    this.MINUS = MINUS
+    };
+
+    // возращает значения дня, часов, минут, секунд для отображения на странице
+    timeComponent() {
     const days = Math.floor(this.deltaTime / (1000 * 60 * 60 * 24));
     const hours = Math.floor((this.deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const mins = Math.floor((this.deltaTime % (1000 * 60 * 60)) / (1000 * 60));
     const secs = Math.floor((this.deltaTime % (1000 * 60)) / 1000);
 
     return {days, hours, mins, secs}
-    },
+    };
 
     // указываем время для обратного отсчета d - дни, h - часы, m - минуты, s - секунды 
     setTimerDay(d){
         return this.endDate.setDate(this.startDate.getDate() + d);
-     },
+     };
 
     setTimerHours(h){
         return this.endDate.setHours(this.startDate.getHours() + h);
-    },
+    };
 
     setTimerMinutes(m){
         return this.endDate.setMinutes(this.startDate.getMinutes() + m);
-    },
+    };
 
     setTimerSeconds(s){
         return this.endDate.setSeconds(this.startDate.getSeconds() + s);
-    },
+    };
 
-    setDeltaTime(d = 2, h = 0, m = 0, s = 0){
+    setTime(d = this.day, h = this.hour, m = this.minutes, s = this.seconds){
         this.setTimerDay(d);
         this.setTimerHours(h);
         this.setTimerMinutes(m);
         this.setTimerSeconds(s);
         return this.deltaTime = this.endDate.getTime() - this.startDate.getTime();
-    },
+    };
 
+    // отнимает от времени указанное значение
     changeDeltaTime(){
        return this.deltaTime -= this.MINUS;
-     },
+     }
 
 }
 
-timer.setDeltaTime(0, 0, 0, 10);
+const timer = new Timer(0,5,0,5,1000);
 
+timer.setTime(); 
 
 const interval = setInterval(() => {
     if (timer.deltaTime !== 0){
@@ -69,11 +79,5 @@ const interval = setInterval(() => {
     refs.mins.textContent = "Finish!";
     refs.secs.textContent = "Finish!";
     clearInterval(interval);
-}
-    
+}   
 }, 1000);
-
-
-const ddd = new Date(2021, 07, 13, 15,00,00);
-
-console.log(ddd);
